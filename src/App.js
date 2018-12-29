7707c2270a8297c3275112bf3a1e14b386fa1ad2
Honey Kanpara <Honey.Kanpara@ril.com>
2018-12-29 21:01:05 +0530

    React-helmet updates for better SEO

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import HomePage from'./homepage/homepage.js'
+import HomePage from'./homepage/homepage.js';
+import {Helmet} from "react-helmet";
 
 class App extends Component {
   
   render() {
     return (
-      <HomePage></HomePage>
+      <div>
+        <Helmet title="About My Location"
+        meta={[
+          {"name": "description", "content": "About my location helps you to find nearby places and their arrress. It is very easy to use and its awesome."},
+         ]}/>
+        {/* <meta charSet="utf-8" />
+        <title>About My Location</title>
+        <link rel="canonical" href="http://mysite.com/example" /> */}
+        <HomePage></HomePage>
+      </div>
     );
   }
 }
