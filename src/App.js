cfa7cc10e5d47f3cf94a180d6c1b3db3d1a76905
Honey Kanpara <Honey.Kanpara@ril.com>
2019-01-02 17:49:26 +0530

    Adding keywords

--- src/App.js
+++ src/App.js
@@ -11,9 +11,11 @@ class App extends Component {
         <Helmet title="About My Location"
         meta={[
           {"name": "description", "content": "About My Location is a basic app which detects your current address and displays near by places on map. Get to know about your location and see important nearby places on map.Happy hunting !!!"},
-          {"name": "google-site-verification", "content": "rh7wBQPuRc8UHq4jNy5RLd-8i1GCRryFTq6XvmfcUf0"}
+          {"name": "google-site-verification", "content": "rh7wBQPuRc8UHq4jNy5RLd-8i1GCRryFTq6XvmfcUf0"},
+          {"name": "keyword", "content": "about my location, about, location, current, my current location, my location, where am i, my coordinate, my gps"}
          ]}/>
          <meta name="google-site-verification" content="rh7wBQPuRc8UHq4jNy5RLd-8i1GCRryFTq6XvmfcUf0" />
+         <meta name="keyword" content="about my location, about, location, current, my current location, my location, where am i, my coordinate, my gps"></meta>
         {/* <meta charSet="utf-8" />
         <title>About My Location</title>
         <link rel="canonical" href="http://mysite.com/example" /> */}
